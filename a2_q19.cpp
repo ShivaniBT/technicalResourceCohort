a157ae831386bc92ab24c6e96c4d80b157cf5fd7
Shivani Bhardwaj <122295533+ShivaniBT@users.noreply.github.com>
2023-01-28 11:48:42 +0530

    submitting  q19 of assignment 2

---
+++ a2_q19.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv){
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==(n/2)+1 ||j==(n/2)+1){
+                cout<<"* ";
+            }
+            else if(i==1){
+                if(j<=(n/2)||j==n){
+                    cout<<"* ";
+                }
+                else{
+                    cout<<"  ";
+                }
+            }
+            else if(i==n){
+                if(j>(n/2)||j==1){
+                    cout<<"* ";
+                }
+                else{
+                    cout<<"  ";
+                }
+            }
+            else if(i<=(n/2)){
+                if(j==n){
+                    cout<<"* ";
+                }
+                else{
+                    cout<<"  ";
+                }
+            }
+            else if(i>(n/2)+1){
+                if(j==1){
+                    cout<<"* ";
+                }
+                else{
+                    cout<<"  ";
+                }
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
